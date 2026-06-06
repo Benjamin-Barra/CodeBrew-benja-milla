4017ff2975cd8553edeef75706a8b1b912d6b25c
06-Ray <millarayarredondovelasquez@gmail.com>
2026-06-05 20:20:35 -0400

    los cafecitos

---
+++ script.js
@@ -0,0 +1,57 @@
+const cafes = [
+    {
+        id:1,
+        nombre: "Espresso",
+        precio: 1000,
+        imagen: "./imgs/espresso.jpg"
+    },
+    {
+        id:2,
+        nombre: "Americano",
+        precio: 1000,
+        imagen: "./imgs/americano.jpg"
+    },
+    {
+        id:3,
+        nombre: "Macchiato",
+        precio: 1000,
+        imagen: "./imgs/macchiato.jpg"
+    },
+    {
+        id:4,
+        nombre: "Cortado",
+        precio: 1000,
+        imagen: "./imgs/cortado.jpg"
+    },
+    {
+        id:5,
+        nombre: "Flat White",
+        precio: 1000,
+        imagen: "./imgs/flat white.jpg"
+    },
+    {
+        id:6,
+        nombre: "Capuccino",
+        precio: 1000,
+        imagen: "./imgs/capuccino.jpg"
+    },
+    {
+        id:7,
+        nombre: "Latte",
+        precio: 1000,
+        imagen: "./imgs/latte.jpg"
+    },
+    {
+        id:8,
+        nombre: "Mocha",
+        precio: 1000,
+        imagen: "./imgs/mocha.jpg"
+    },
+    {
+        id:9,
+        nombre: "Iced Latte",
+        precio: 1000,
+        imagen: "./imgs/iced latte.jpg"
+    }
+
+]
